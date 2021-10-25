665dc2ef20a7b3b9a26dc1e791e630fb5980c381
EinNiemand28 <Ein_Niemand@foxmail.com>
2021-10-25 22:55:48 +0800

    第一次提交

--- js/runtime.js
+++ js/runtime.js
@@ -27,10 +27,10 @@ setInterval(() => {
     time[4] = nol(second);
 }
   if ((Number(time[2])<22) && (Number(time[2])>7)){
-    currentTimeHtml ="<img class='boardsign' src='https://cdn.jsdelivr.net/gh/EinNiemand28/my-img@master/images/营业中.jpg' title='距离百年网站也就差不到一百年~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+    currentTimeHtml ="<img class='boardsign' src='https://cdn.jsdelivr.net/gh/EinNiemand28/my-img@master/images/营业.jpg' title='距离百年网站也就差不到一百年~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
   }
   else{
-    currentTimeHtml ="<img class='boardsign' src='https://cdn.jsdelivr.net/gh/EinNiemand28/my-img@master/images/打烊了.jpg' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+    currentTimeHtml ="<img class='boardsign' src='https://cdn.jsdelivr.net/gh/EinNiemand28/my-img@master/images/打烊.jpg' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
 }
   document.getElementById("workboard").innerHTML = currentTimeHtml;
 }, 1000);
